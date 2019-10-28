df7d1f0e62a95b0d3c0b55d70877c36f6585f000
Arjun Singh <asingh6766@altimetrik.com>
2019-10-28 22:54:30 +0530

    new java round

---
+++ src/box/test/javaSecondRound/ArrayListConcept.java
@@ -0,0 +1,31 @@
+package box.test.javaSecondRound;
+
+import java.util.ArrayList;
+
+public class ArrayListConcept {
+
+	public static void main(String[] args) {
+		
+		ArrayList ar=new ArrayList();
+		
+		System.out.println(ar.size());
+		ar.add(100);
+		ar.add(200);
+		
+		System.out.println(ar.size());
+		
+		ar.add(12.33);
+		ar.add(23.44);
+		
+		System.out.println(ar.size());
+		
+		ar.add("Test");
+		ar.add("Selenium");
+		
+		System.out.println(ar.size());
+		
+		
+
+	}
+
+}
